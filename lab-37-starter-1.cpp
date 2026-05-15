f9734155bdbc948326ee288d48380f29fa32ec11
Amogh Nadakudhiti <amogh.nad@gmail.com>
2026-05-14 18:46:17 -0700

    Milestone 1

---
+++ lab-37-starter-1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//prototype for func
+int sum_ascii(string);
+
+int main() {
+    char a = 'A';
+    cout << a << endl;
+    cout << (int) a << endl;
+    int b = 66;
+    cout << b << endl;
+    cout << (char) b << endl;
+    cout << sum_ascii("yooo");
+
+    return 0;
+}
+
+int sum_ascii(string s){
+    int sum = 0;
+    int sz = s.size();
+    for (int i = 0; i < sz; i++){
+        sum += (int) s[i];
+    }
+
+    return sum;
+}
+
+/* 
+These targets are present in the dataset and can be used for testing:
+536B9DFC93AF
+1DA9D64D02A0
+666D109AA22E
+E1D2665B21EA
+*/
